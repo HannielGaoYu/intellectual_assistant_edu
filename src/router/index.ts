8de6876269835a10f2ac625f1f3fae3578b74cf8
phli_angel <3321029956@qq.com>
2024-06-21 20:58:42 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: []
+})
+
+export default router
